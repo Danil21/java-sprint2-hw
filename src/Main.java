0305fbc3a993148c4f3b39ccc46dece28376c6ee
Daniil Vagin <31821166+Danil21@users.noreply.github.com>
2023-07-20 16:45:38 +0000

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Поехали!
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+            String commandExt = scanner.nextLine();
+
+            if (command == 1) {
+                int currency = scanner.nextInt();
+
+            } else if (command == 2) {
+              
+            } else if (command == 3) {
+
+            } else if (command == 4) {
+              
+            } else if (command == 5) {
+
+            } else if (command == "пока") {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
     }
-}
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Считать все месячные отчёты");
+        System.out.println("2 - Считать годовой отчёт");
+        System.out.println("3 - Сверить отчёты");
+        System.out.println("4 - Вывести информацию обо всех месячных отчётах");
+        System.out.println("5 - Вывести информацию о годовом отчёте");
+        System.out.println("Введите 'пока' для завершения программы");
+    }
+ }
+
 
